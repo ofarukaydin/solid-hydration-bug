eecfcc7f3ff92a1b56f43b4c108da4c7a9671703
Faruk Aydin <farukaydin@outlook.com>
2023-11-03 23:20:36 +0300

    vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import cloudflare from "solid-start-cloudflare-pages";
+// import cloudflare from "solid-start-cloudflare-pages";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid({ ssr: true, adapter: cloudflare({}) })],
+  plugins: [solid({ ssr: true, adapter: vercel({ edge: true }) })],
 });
